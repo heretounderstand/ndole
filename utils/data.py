5912668a7b16c6a94aee53b01660543f2daccecf
heretounderstand <99259260+heretounderstand@users.noreply.github.com>
2025-06-06 13:48:53 +0100

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -1,8 +1,5 @@
 import streamlit as st
-from streamlit_cookies_controller import CookieController
 from supabase import create_client, Client
-
-controller = CookieController()
 
 @st.cache_resource
 def initialize_supabase() -> Client:
